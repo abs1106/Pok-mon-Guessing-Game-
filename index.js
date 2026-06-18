a4a58dd6497dccef7f8ee1d147eda8f9d58223a3
Arwa Baig <arwabaig786@gmail.com>
2026-06-18 11:32:19 -0400

    added hint restraint and now adding in hints

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 
 const app = express();
-
+app.use(express.json());
 
 const games = {};
 
@@ -10,27 +10,45 @@ app.get("/start/",  async (req, res) => {
    const pokemonId = Math.floor(Math.random() * 151)+1;// generate pokemonId
   const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)// fetch pokemom
   const pokemonData = await pokemonResponse.json();// store game
+  // const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)// fetch pokemom
+  // const pokemonData = await pokemonResponse.json();// store game
+
+  // const pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)// fetch pokemom
+  // const pokemonData = await pokemonResponse.json();// store game
+
     games[gameId] = {
     pokemonName: pokemonData.name,
     pokemonId,
     hints: 5,
-    hintsUsed: 0
+    hintsUsed: 0,
+    firstLetter: pokemonData.name[0],
+    ability: pokemonData.abilities[0].ability.name,
+    type: pokemonData.types[0].type.name,
   };
   res.send("You game ID is " + gameId);// send response which is your game ID
 });
 
-app.listen(4000);
 
-app.post("/guess"), (req, res) => {
+app.post("/guess", (req, res) => {
     const gameId = req.body.gameId
     const guess = req.body.guess
     const game = games[gameId]
-    
-     return res.status(404).json({ error: "GameId doesnt exist " });
 
-    if (guess === game.pokemonName)
-        res.send("You guessed correctly!");
-    {
+      if (!game) {
+        return res.status(404).json({ error: "GameId doesnt exist " });
+      }
+
+      if (guess === game.pokemonName){
+        return res.send("You guessed correctly!");
+
+      }else{
+        game.hintsUsed++;
+
+          if(game.hintsUsed === game.hints){
+            return res.send("Too many guesses the Pokemon was: " + game.pokemonName  )
+
+
+        }
         
     }
         
@@ -38,4 +56,5 @@ app.post("/guess"), (req, res) => {
 
 
 
-  }
+  });
+app.listen(4000);
